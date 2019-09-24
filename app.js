07c114eda35bbec24278b11c3c9e331922f8a4c7
王林 <wb-wl511449@alibaba-inc.com>
2019-09-24 18:26:56 +0800

    a

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ var Api = require("/config/api.js");
 
 App({
   onLaunch: function () {
-    // 查询本地是否有值，如果有值不需要登录，如果没有值，判断是否授权，如果未授权跳转login如果已授权 在本页面执行默认登录用户的逻辑
+    /**查询本地是否有值，
+     * 如果有值不需要登录，
+     * 如果没有值，
+     *    判断是否授权，
+     *    如果未授权：跳转login
+     *    如果已授权：在本页面执行默认登录用户的逻辑
+     */
     wx.getStorage({
       key: 'token',
-      success: function(res) {},
-      fail: function(e) {
+      fail: function() {
         wx.reLaunch({
           url: '/pages/login/index',
         })
       },
     });
   },
-  
+
   globalData: {
     userInfo: null
   },

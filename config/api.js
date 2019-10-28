73f6f16992b6a7f2f7d4880c25dd2c78222f3945
xiaohuangandxiaobai <dinghx2019@163.com>
2019-10-28 08:21:50 +0800

    canvas

--- config/api.js
+++ config/api.js
@@ -62,4 +62,5 @@ module.exports = {
   MessagePraise: ApiRootUrl + 'message/support', //点赞消息
   MessageComment: ApiRootUrl + 'message/evaluate', //评论消息
   MessageSystem: ApiRootUrl + 'message/system', //评论消息
+  getImg: ApiRootUrl+ '/users/RandWord', // 获取分享生成图的随机背景图
 }
